0fe1e52c902414a413bca620c35fdb378ff946df
GeorgiosPapouts <georgios.papoutsidakis@ucdconnect.ie>
2021-03-01 23:30:24 +0000

    last changes to Week 3

--- Week 3_UCD_GP.py
+++ Week 3_UCD_GP.py
@@ -11,4 +11,46 @@ print(cars[['country']])
 # Print out DataFrame with country and drives_right columns
 print(cars[['country', 'drives_right']])
 
-uguygyl
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out first 3 observations
+print(cars[0:3])
+
+# Print out fourth, fifth and sixth observation
+print(cars[3:6])
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out observation for Japan
+print(cars.iloc[2])
+
+# Print out observations for Australia and Egypt
+print(cars.iloc[[1,6]])
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out drives_right value of Morocco
+print(cars.loc['MOR'])
+
+# Print sub-DataFrame
+print(cars.iloc[[4, 5], [1, 2]])
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out drives_right column as Series
+print(cars.iloc[:,2])
+
+# Print out drives_right column as DataFrame
+print(cars.iloc[:,[2]])
+
+# Print out cars_per_cap and drives_right as DataFrame
+print(cars.iloc[:, [0,2]])
+
