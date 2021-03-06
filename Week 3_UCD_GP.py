99304c3e79f401dd15dc23fcfbebdac596138d00
GeorgiosPapouts <georgios.papoutsidakis@ucdconnect.ie>
2021-03-06 01:19:36 +0000

    Friday changes to Week 3

--- Week 3_UCD_GP.py
+++ Week 3_UCD_GP.py
@@ -254,3 +254,45 @@ areas = [11.25, 18.0, 20.0, 10.75, 9.50]
 for index, area in enumerate(areas) :
     print("room " + str(index+1) + ": " + str(area))
 
+for x in house :
+    print("the " + x[0] + " is " + str(x[1]) + " sqm")
+
+# Definition of dictionary
+europe = {'spain': 'madrid', 'france': 'paris', 'germany': 'berlin',
+          'norway': 'oslo', 'italy': 'rome', 'poland': 'warsaw', 'austria': 'vienna'}
+
+# Iterate over europe
+for key, value in europe.items():
+    print('the capital of' + str(key) + 'is' + str(value))
+
+# Import numpy as np
+import numpy as np
+
+# For loop over np_height
+for x in np_height:
+    print(str(x) + 'inches')
+
+# For loop over np_baseball
+for x in np.nditer(np_baseball):
+    print(x)
+
+# Import numpy as np
+import numpy as np
+
+# For loop over np_height
+for x in np_height :
+    print(str(x) + " inches")
+
+# For loop over np_baseball
+for x in np.nditer(np_baseball) :
+    print(x)
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Iterate over rows of cars
+for lab, row in cars.iterrows():
+    print(lab)
+    print(row)
+
