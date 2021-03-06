de5cbb28d8d69bb8813ba52412ebbadbe4bc8201
GeorgiosPapouts <georgios.papoutsidakis@ucdconnect.ie>
2021-03-06 19:24:14 +0000

    Saturday 1 changes to Week 3

--- Week 3_UCD_GP.py
+++ Week 3_UCD_GP.py
@@ -291,8 +291,36 @@ for x in np.nditer(np_baseball) :
 import pandas as pd
 cars = pd.read_csv('cars.csv', index_col = 0)
 
-# Iterate over rows of cars
-for lab, row in cars.iterrows():
-    print(lab)
-    print(row)
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Adapt for loop
+for lab, row in cars.iterrows() :
+    print(lab + ': ' + str(row['cars_per_cap']))
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Code for loop that adds COUNTRY column
+for lab, row in cars.iterrows() :
+    cars.loc[lab, "COUNTRY"] = row["country"].upper()
+
+
+# Print cars
+print(cars)
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Use .apply(str.upper)
+for lab, row in cars.iterrows() :
+    cars.loc[lab, "COUNTRY"] = row["country"].upper()
+
+    print(cars)
+
+
+
 
