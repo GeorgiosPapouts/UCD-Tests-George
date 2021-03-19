f4fece425bd61a84ca4d82f359b4ac0696019a83
GeorgiosPapouts <georgios.papoutsidakis@ucdconnect.ie>
2021-03-19 11:34:36 +0000

    19th March changes

--- Week 5_UCD_GP.py
+++ Week 5_UCD_GP.py
@@ -181,3 +181,12 @@ a_tags = soup.find_all('a')
 for link in a_tags:
     print(link.get('href'))
 
+
+# Load JSON: json_data
+with open("a_movie.json") as json_file:
+    json_data = json.load(json_file)
+
+# Print each key-value pair in json_data
+for k in json_data.keys():
+    print(k + ': ',json_data[k])
+
