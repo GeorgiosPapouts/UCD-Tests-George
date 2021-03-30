dee5e697d2d4c9be2db2804029391d79fd19e5ec
georgepapoutsidakis <georgios.papoutsidakis@ucdconnect.ie>
2021-03-30 23:01:53 +0100

    30th March changes to week 7

--- Week 7_UCD _GP.py
+++ Week 7_UCD _GP.py
@@ -43,3 +43,44 @@ wards_census_altered = wards.merge(census_altered, on='ward')
 print('wards_census_altered table shape:', wards_census_altered.shape)
 
 
+# Merge the licenses and biz_owners table on account
+licenses_owners = licenses.merge(biz_owners)
+
+# Group the results by title then count the number of accounts
+counted_df = licenses_owners.groupby('title').agg({'account':'count'})
+
+# Sort the counted_df in desending order
+sorted_df = counted_df.sort_values('account', ascending=False)
+
+# Use .head() method to print the first few rows of sorted_df
+print(sorted_df.head())
+
+# Merge the ridership and cal tables
+ridership_cal = ridership.merge(cal)
+
+# Merge the ridership, cal, and stations tables
+ridership_cal_stations = ridership.merge(cal, on=['year','month','day']) \
+            				.merge(stations, on=['station_id'])
+
+
+# Merge the ridership, cal, and stations tables
+ridership_cal_stations = ridership.merge(cal, on=['year','month','day']) \
+							.merge(stations, on='station_id')
+
+# Create a filter to filter ridership_cal_stations
+filter_criteria = ((ridership_cal_stations['month'] == 7)
+                   & (ridership_cal_stations['day_type'] == 'Weekday')
+                   & (ridership_cal_stations['station_name'] == 'Wilson'))
+
+# Use .loc and the filter to select for rides
+print(ridership_cal_stations.loc[filter_criteria, 'rides'].sum())
+
+# Merge licenses and zip_demo, on zip; and merge the wards on ward
+licenses_zip_ward = licenses.merge(zip_demo, on='zip') \
+            			.merge(wards, on='ward')
+
+# Print the results by alderman and show median income
+print(licenses_zip_ward.groupby('alderman').agg({'income':'median'}))
+
+
+
