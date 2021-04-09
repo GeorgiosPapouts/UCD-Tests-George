097e015bf2160229e7874a7ee31646c94dbaf901
georgepapoutsidakis <georgios.papoutsidakis@ucdconnect.ie>
2021-04-09 09:42:48 +0100

    9th April changes to Week 8

--- Week 8_UCD_GP.py
+++ Week 8_UCD_GP.py
@@ -343,3 +343,17 @@ fig, ax = plt.subplots()
 ax.plot(austin_weather["MONTH"], austin_weather["MLY-TAVG-NORMAL"])
 plt.show()
 
+
+# Save as a PNG file
+fig.savefig('my_figure.png')
+
+# Save as a PNG file with 300 dpi
+fig.savefig('my_figure_300dpi.png', dpi=300)
+
+fig.set_size_inches([3, 5])
+fig.savefig('figure_3_5.png')
+
+fig.set_size_inches([5, 3])
+fig.savefig('figure_5_3.png')
+
+
