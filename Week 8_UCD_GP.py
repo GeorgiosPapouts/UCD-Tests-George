fae187b159d0beef7934f65e196dc94154cb30aa
georgepapoutsidakis <georgios.papoutsidakis@ucdconnect.ie>
2021-04-09 20:24:08 +0100

    9th April changes to Week 8-2

--- Week 8_UCD_GP.py
+++ Week 8_UCD_GP.py
@@ -357,3 +357,29 @@ fig.set_size_inches([5, 3])
 fig.savefig('figure_5_3.png')
 
 
+# Extract the "Sport" column
+sports_column =summer_2016_medals['Sport']
+
+# Find the unique values of the "Sport" column
+sports = summer_2016_medals['Sport'].unique()
+
+# Print out the unique sports values
+print(sports)
+
+
+fig, ax = plt.subplots()
+
+# Loop over the different sports branches
+for sport in sports:
+  # Extract the rows only for this sport
+  sport_df = summer_2016_medals[summer_2016_medals['Sport']==sport]
+  # Add a bar for the "Weight" mean with std y error bar
+  ax.bar(sport, sport_df['Weight'].mean(), yerr=sport_df['Weight'].std())
+
+ax.set_ylabel("Weight")
+ax.set_xticklabels(sports, rotation=90)
+
+# Save the figure to file
+fig.savefig('sports_weights.png')
+
+
