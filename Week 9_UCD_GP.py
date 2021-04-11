917aa6f818b5c56b5af05d06c39ffabf9effc3ef
georgepapoutsidakis <georgios.papoutsidakis@ucdconnect.ie>
2021-04-11 18:42:41 +0100

    11th April changes to Week 9-2

--- Week 9_UCD_GP.py
+++ Week 9_UCD_GP.py
@@ -119,3 +119,34 @@ plt.show()
 
 sns.palplot(sns.color_palette('coolwarm', 6))
 plt.show()
+
+
+# Create a figure and axes
+fig, ax = plt.subplots()
+
+# Plot the distribution of data
+sns.distplot(df['fmr_3'], ax=ax)
+
+# Create a more descriptive x axis label
+ax.set(xlabel="3 Bedroom Fair Market Rent")
+
+# Show the plot
+plt.show()
+
+
+# Create a figure and axes
+fig, ax = plt.subplots()
+
+# Plot the distribution of 1 bedroom rents
+sns.distplot(df['fmr_1'], ax=ax)
+
+# Modify the properties of the plot
+ax.set(xlabel="1 Bedroom Fair Market Rent",
+       xlim=(100, 1500),
+       ylabel="US Rent")
+
+# Display the plot
+plt.show()
+
+
+
